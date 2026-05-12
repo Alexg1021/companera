122ee3480828b6004e02613168026c2926994c91
Alex Gutierrez <alexgutierrez@Alexs-MacBook-Pro-2.local>
2026-05-12 03:08:30 -0700

    adding about html

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      { source: "/brief", destination: "/companera_brief.html" },
+    ];
+  },
+};
 
 export default nextConfig;
